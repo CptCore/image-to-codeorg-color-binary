41f8546fde2c853d692e1093bebac08e370cebc9
TheSuitedWaffle <69922745+CptCore@users.noreply.github.com>
2024-09-23 16:50:36 -0700

    it works :)

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from PIL import Image
 import numpy as np
+
 filename =  input("Enter the Image name (Very Case Sensitive) : ")
 with Image.open(filename) as img:
-    img.load()
-
-img = img.convert("RGB")
+    img = img.convert("RGB")
+    img = img.resize ((75,75))
 
 temprgbnum = 24
 imagebinary = ""
-imagebinary += str(img.size[0].to_bytes(1,byteorder='big'))
-imagebinary += str(img.size[1].to_bytes(1,byteorder='big'))
-imagebinary += str(temprgbnum.to_bytes(1,byteorder='big'))
+imagebinary += ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in img.size[0].to_bytes(1,byteorder='big'))
+imagebinary += ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in img.size[1].to_bytes(1,byteorder='big'))
+imagebinary += ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in temprgbnum.to_bytes(1,byteorder='big'))
 
-image_array = np.array(img)
-image_array = image_array.tobytes()
-print(image_array[:10])
+image_array = list(img.getdata())
+
+for pixel in image_array:
+    r = ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in pixel[0].to_bytes(1,byteorder='big'))
+    g = ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in pixel[1].to_bytes(1,byteorder='big'))
+    b = ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in pixel[2].to_bytes(1,byteorder='big'))
+    rgb = r + g + b
+    print(r,g,b)
+    imagebinary += rgb
+
+with open("output.txt", "w") as text_file:
+    # Write the string to the file
+    text_file.write(imagebinary)
+    print("Saved to output.txt :3")
+
+# Convert the RGB data to bytes
+#image_array = bytes([value for pixel in image_array for value in pixel])
+#image_array = ''.join(''.join(str((byte >> i) & 1) for i in range(7, -1, -1)) for byte in image_array)
+#print(image_array)
