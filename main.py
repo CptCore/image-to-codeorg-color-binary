d1ecff773f811b15216f7376da13a4132cb06e09
TheSuitedWaffle <69922745+CptCore@users.noreply.github.com>
2024-09-23 14:30:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ img = img.convert("RGB")
 
 temprgbnum = 24
 imagebinary = ""
-imagebinary += img.size[0].to_bytes(1,byteorder='big') --convert to string somehow??
-imagebinary += img.size[1].to_bytes(1,byteorder='big')
-imagebinary += temprgbnum.to_bytes(1,byteorder='big')
+imagebinary += str(img.size[0].to_bytes(1,byteorder='big'))
+imagebinary += str(img.size[1].to_bytes(1,byteorder='big'))
+imagebinary += str(temprgbnum.to_bytes(1,byteorder='big'))
 
 image_array = np.array(img)
 image_array = image_array.tobytes()
